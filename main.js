71c9bffc7684f1bc8401d5e628b0b3adc1172749
George Meskhoradze <116169423+George-Meskhoradze@users.noreply.github.com>
2023-02-17 21:50:59 +0400

    added responsive

--- main.js
+++ main.js
@@ -9,54 +9,49 @@ let playerScoreCount = 0;
 let computerScoreCount = 0;
 
 
-
 const playerScoreValue = localStorage.getItem("playerScore");
 if (playerScoreValue !== null) {
   playerScoreCount = parseInt(playerScoreValue);
   playerScore.textContent = playerScoreCount;
-  if(playerScoreCount >= 3){
+
+  if(playerScoreCount == 3){
     playerScoreCount = 0
     computerScoreCount = 0
     localStorage.setItem('playerScore', playerScoreCount);
     localStorage.setItem('computerScore', computerScoreCount);
     playerScore.textContent = playerScoreCount;
     computerScore.textContent = computerScoreCount;
+    }
   }
-  
-}
+
+
 
 const computerScoreValue = localStorage.getItem("computerScore");
 if (computerScoreValue !== null) {
   computerScoreCount = parseInt(computerScoreValue);
   computerScore.textContent = computerScoreCount;
-  if(computerScoreCount >= 3){
+
+  if(computerScoreCount == 3){
     playerScoreCount = 0
     computerScoreCount = 0
     localStorage.setItem('playerScore', playerScoreCount);
     localStorage.setItem('computerScore', computerScoreCount);
     playerScore.textContent = playerScoreCount;
     computerScore.textContent = computerScoreCount;
+    console.log('player score equals 3');
   }
-  
-  
 }
 
 
 
+
 const SELECTIONS = [
-  {
-    name: 'rock',
-  },
-  {
-    name: 'paper',
-  },
-  {
-    name: 'scissors',
-  }
+  {name: 'rock'},
+  {name: 'paper'},
+  {name: 'scissors'}
 ]
 
 let computerGame;
-
 
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function Handler() {
@@ -73,22 +68,19 @@ for (let i = 0; i < buttons.length; i++) {
     }
     computerTurn();
     Winner();
-    refreshGame();
   });
 };
-
 
 function makeSelection(selection) {
   console.log(selection)
 }
-
-
 
 function Winner() {
   let playerMe = document.querySelector(".images [data-selection]:not([style*='display: none'])");
   let playerSelectionName = playerMe.dataset.selection;
   let computerSelectionName = computerGame.dataset.selection;
   let result;
+
 
   if (playerSelectionName  === computerSelectionName) {
     playerMe.style.background = 'var(--win)'
@@ -117,18 +109,9 @@ function Winner() {
 
 }
 
-
-function refreshGame() {
-  btn.addEventListener("click", ()=> {
-    window.location.reload()
-    })
-}
-
-
-
-  // function gameOver() {
-
-// }
+btn.addEventListener('click', ()=> {
+  window.location.reload()
+})
 
 
 function computerTurn() {
@@ -150,4 +133,33 @@ function computerTurn() {
 
 
 
+if (window.matchMedia("(max-width:1200px)").matches) {
+  // code for handling clicks on buttons for mobile devices
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function Handler() {
+      buttons[i].style.transform = 'translateX(80px)'
+      computerGame.style.transform = "translateX(-80px)";
+    });
+  }
+} 
 
+
+if (window.matchMedia("(max-width:700px)").matches) {
+  // code for handling clicks on buttons for mobile devices
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function Handler() {
+      buttons[i].style.transform = 'translateX(40px)'
+      computerGame.style.transform = "translateX(-40px)";
+    });
+  }
+}
+
+if (window.matchMedia("(max-width:430px)").matches) {
+  // code for handling clicks on buttons for mobile devices
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function Handler() {
+      buttons[i].style.transform = 'translateX(15px)'
+      computerGame.style.transform = "translateX(-15px)";
+    });
+  }
+}
